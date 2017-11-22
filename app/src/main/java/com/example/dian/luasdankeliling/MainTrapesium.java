d6e301422f8f6654d6dcd06e91c8ab10fc498563
Dian <diannovitasari127@gmail.com>
2017-11-22 13:16:49 +0700

    UTS

---
+++ app/src/main/java/com/example/dian/luasdankeliling/MainTrapesium.java
@@ -0,0 +1,14 @@
+package com.example.dian.luasdankeliling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainTrapesium extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_trapesium);
+
+    }
+}
