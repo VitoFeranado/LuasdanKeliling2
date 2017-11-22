7847580641c26f0442d6535b7f68463a191c3843
vitoferanado <vitofernando15@gmail.com>
2017-11-22 13:49:56 +0700

    UTS

--- app/src/main/java/com/example/dian/luasdankeliling/HasilSegitiga.java
+++ app/src/main/java/com/example/dian/luasdankeliling/HasilSegitiga.java
@@ -1,14 +1,20 @@
 package com.example.dian.luasdankeliling;
 
-
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class HasilSegitiga extends AppCompatActivity {
-       @Override
+    TextView tv;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil_segitiga);
 
+        tv = (TextView)findViewById(R.id.tv);
+        Intent i = getIntent();
+        String nilai = i.getStringExtra("nilai");
+        tv.setText(nilai);
     }
 }
